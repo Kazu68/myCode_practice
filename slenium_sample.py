eee74f84d25193405e444bc7961c2071eb4fa2eb
Kazu68 <oka.nobu5rh@gmail.com>
2021-11-27 09:02:48 +0900

    added selenium sample code

---
+++ slenium_sample.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+ 
+CHROMEDRIVER = "C:\data\chromedriver\chromedriver.exe"
+ 
+# ドライバー指定でChromeブラウザを開く
+browser = webdriver.Chrome(CHROMEDRIVER)
+ 
+# Googleアクセス
+browser.get('https://www.google.com/')
+ 
+# 検索ボックスを特定
+elem = browser.find_element(By.NAME, 'q')
+# 「Selenium」と入力して、「Enter」を押す
+elem.send_keys('python' + Keys.RETURN)
